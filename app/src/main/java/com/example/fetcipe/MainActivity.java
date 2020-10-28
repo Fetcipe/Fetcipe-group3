614562abed5a74f0bd2af4c23062ec2f7ad9b0e4
Thu Tran <edm.4thmvt@gmail.com>
2020-10-28 15:52:21 +0700

    gvgvg

--- app/src/main/java/com/example/fetcipe/MainActivity.java
+++ app/src/main/java/com/example/fetcipe/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "sahdashd", Toast.LENGTH_SHORT).show();
+       
     }
 }
