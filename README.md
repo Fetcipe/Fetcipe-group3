34d08d1ed10ed7ff774156ef6f9e7457439a3c34
Thu Tran 19bit <1959039@itec.hcmus.edu.vn>
2020-11-02 23:21:02 +0700

    Interface for Recipe posting
    - Current interface for user when viewing the recipe, including add comment bubble
    - Phan giao dien ng dung khi doc cong thuc, co phan them binh luan o duoi
    
    + Need posting/Can bai post

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+<<<<<<< Updated upstream
 CoinMan
+=======
+---Day la phan binh luan cua app---
+
+Cai can thiet:
+1 interface co day du cac thu:
+- bai post
+
+Phan hien tai se lam comment + rating
+>>>>>>> Stashed changes
---
+++ app/src/main/java/com/example/fetcipe/CommentActivity.java
@@ -0,0 +1,13 @@
+package com.example.fetcipe;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CommentActivity {
+    Button addComment;
+    EditText editTextComment;
+
+    public CommentActivity() {
+    }
+
+}
