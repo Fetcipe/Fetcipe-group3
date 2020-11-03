af89a991d579ba1bdf765d62734562aa5384a55d
Thu Tran 19bit <1959039@itec.hcmus.edu.vn>
2020-11-03 13:18:46 +0700

    day1

--- app/src/main/java/com/example/fetcipe/Activities/MainActivity.java
+++ app/src/main/java/com/example/fetcipe/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.fetcipe;
+package com.example.fetcipe.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.fetcipe.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/fetcipe/Models/Post.java
@@ -0,0 +1,4 @@
+package com.example.fetcipe.Models;
+
+public class Post {
+}
