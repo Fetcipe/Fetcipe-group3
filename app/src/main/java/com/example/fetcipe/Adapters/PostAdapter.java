00c4433cd7c9adb38a462f6ce3d84f032a10cbc6
Thu Tran 19bit <1959039@itec.hcmus.edu.vn>
2020-11-05 22:03:05 +0700

    user pick avatar
    user can pick their avatar from gallery now

--- app/src/main/java/com/example/fetcipe/Adapters/PostAdapter.java
+++ app/src/main/java/com/example/fetcipe/Adapters/PostAdapter.java
@@ -1,12 +1,17 @@
 package com.example.fetcipe.Adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fetcipe.Models.Post;
+import com.example.fetcipe.R;
 
 import java.util.List;
 
@@ -23,16 +28,35 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.MyViewHolder> 
     @NonNull
     @Override
     public PostAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View row= LayoutInflater.from(mContext).inflate(R.layout.row_post_item, false);
+        return new MyViewHolder(row);
     }
 
     @Override
     public void onBindViewHolder(@NonNull PostAdapter.MyViewHolder holder, int position) {
 
+        holder.tvTitle.setText(mData.get(position).getTitle());
+        Glide.with(mContext).load(mData.get(position).getPicture()).info(holder.imgPost);
+        Glide.with(mContext).load(mData.get(position).getUserPhoto()).info(holder.imgPostProfile);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mData.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView tvTitle;
+        ImageView imgPost;
+        ImageView imgPostProfile;
+
+        public MyViewHolder(View itemView){
+            super(itemView);
+
+            tvTitle=itemView.findViewById(R.id.row_post_title);
+            imgPost=itemView.findViewById(R.id.row_post_img);
+            imgPostProfile=itemView.findViewById(R.id.row_post_profile_img);
+        }
     }
 }
