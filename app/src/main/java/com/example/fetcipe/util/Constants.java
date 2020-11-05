89a462e448de8a7c267a1a1ce32a3ba138338f5d
Thư <1759038@itec.hcmus.edu.vn>
2020-11-05 22:38:23 +0700

    add retrofit, apikey and source to get data

---
+++ app/src/main/java/com/example/fetcipe/util/Constants.java
@@ -0,0 +1,8 @@
+package com.example.fetcipe.util;
+
+public class Constants {
+
+    public static final String BASE_URL="https://spoonacular.com/";
+    //THIS JUST CAN CALL LIMITS TIME PER DAY, REPLACE ANOTHER KEY IF IT OUT OF TIME
+    public static final String API_KEY="ed25b1f186c544cdb1e0ac1b39470f9a";
+}
