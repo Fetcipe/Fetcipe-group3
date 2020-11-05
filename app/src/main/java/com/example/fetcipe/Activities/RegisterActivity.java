1f50444c021852ab1ecb0544b8213ec41a1aeaa6
Thu Tran 19bit <1959039@itec.hcmus.edu.vn>
2020-11-05 22:03:05 +0700

    dummy

---
+++ app/src/main/java/com/example/fetcipe/Activities/RegisterActivity.java
@@ -0,0 +1,92 @@
+package com.example.fetcipe.Activities;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.fetcipe.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    ImageView ImgUserPhoto;
+    static int PReqCode = 1;
+    static int REQUESTCODE = 1;
+    Uri pickedImgUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        //ini view
+        ImgUserPhoto =findViewById(R.id.regUserPhoto);
+
+        ImgUserPhoto.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View view) {
+                if(Build.VERSION.SDK_INT>=22){
+                    checkAndRequestForPermission();
+
+                }
+                else{
+
+                openGallery();
+
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode==RESULT_OK && requestCode == REQUESTCODE && data != null){
+            //the user has successfully picked the image
+            //save its reference to a Uri variable
+            pickedImgUri = data.getData();
+            ImgUserPhoto.setImageURI(pickedImgUri);
+        }
+    }
+
+    private void openGallery() {
+        //open gallery intent and wait for user to pick an image
+
+        Intent galleryIntent=new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,REQUESTCODE);
+
+
+
+    }
+
+    //request for access the image storage
+    private void checkAndRequestForPermission() {
+        if(ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Toast.makeText(RegisterActivity.this,"Please accept for required permission",Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(RegisterActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},PReqCode);
+            }
+
+        }
+        else
+            openGallery();
+    }
+}
