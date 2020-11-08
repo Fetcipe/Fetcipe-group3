092c9d1f13ec997b858b3a62362a756cb382a3c0
Thu Tran 19bit <1959039@itec.hcmus.edu.vn>
2020-11-08 16:46:41 +0700

    user can upload their avatar and name to FB storage

--- app/src/main/java/com/example/fetcipe/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/fetcipe/Activities/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.fetcipe.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -13,10 +14,18 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.fetcipe.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -25,12 +34,57 @@ public class RegisterActivity extends AppCompatActivity {
     ImageView ImgUserPhoto;
     Uri pickedImgUri;
 
+    private EditText userEmail, userPassword, userPassword2, userName;
+    private ProgressBar loadingProgress;
+    private Button regBtn;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
         //ini view
+        userEmail = findViewById(R.id.regMail);
+        userPassword = findViewById(R.id.regPassword);
+        userPassword2 = findViewById(R.id.regPassword2);
+        userName = findViewById(R.id.regName);
+        loadingProgress = findViewById(R.id.regProgressBar);
+        regBtn = findViewById(R.id.regBtn);
+        loadingProgress.setVisibility(View.INVISIBLE);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                regBtn.setVisibility(View.INVISIBLE);
+                loadingProgress.setVisibility(View.INVISIBLE);
+                final String email = userEmail.getText().toString();
+                final String password = userPassword.getText().toString();
+                final String password2 = userPassword2.getText().toString();
+                final String name = userName.getText().toString();
+
+
+                if (email.isEmpty() || name.isEmpty() || password.isEmpty() ||
+                        !password.equals(password2)) {
+                    //show error: all fields must be filled
+                    //display message
+                    showMessage("Please fill all fields");
+                    regBtn.setVisibility(View.VISIBLE);
+                    loadingProgress.setVisibility(View.INVISIBLE);
+
+                } else {
+                    //everything is okay and all fields are filled
+                    //start creating user account
+                    //createUserAccount will try to create the user if the email is valid
+
+                    createUserAccount(email, name, password);
+                }
+            }
+        });
+
         ImgUserPhoto = findViewById(R.id.regUserPhoto);
 
         ImgUserPhoto.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +100,46 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void createUserAccount(String email, final String name, String password) {
+
+        //this method create user account with specific email and password
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            //user account created successfully
+                            showMessage("Account created successfully");
+                            //after created user account we need to update profile picture and name
+                            updateUserInfo(name, pickedImgUri, mAuth.getCurrentUser());
+                        } else {
+
+                            //account creation failed
+                            showMessage("Account created failed" + task.getException().getMessage());
+                            regBtn.setVisibility(View.VISIBLE);
+                            loadingProgress.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+
+
+    }
+
+    //update user photo and name
+    private void updateUserInfo(String name, Uri pickedImgUri, FirebaseUser currentUser) {
+
+        //first we need to upload user photo to firebase storage
+        //and get uri
+
+
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+
     }
 
     @Override
