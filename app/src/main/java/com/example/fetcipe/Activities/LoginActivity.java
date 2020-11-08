a6900d0cc26fcdef8d17b5d05ac246dbdf560402
Thu Tran 19bit <1959039@itec.hcmus.edu.vn>
2020-11-09 05:10:51 +0700

    updating manifest and creating methods for login activity

---
+++ app/src/main/java/com/example/fetcipe/Activities/LoginActivity.java
@@ -0,0 +1,91 @@
+package com.example.fetcipe.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.fetcipe.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText userMail, userPassword;
+    private Button btnLogin;
+    private ProgressBar loginProgress;
+    private FirebaseAuth mAuth;
+    private Intent HomeActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        userMail = findViewById(R.id.login_mail);
+        userPassword = findViewById(R.id.login_password);
+        btnLogin = findViewById(R.id.loginBtn);
+        loginProgress = findViewById(R.id.login_progress);
+        mAuth = FirebaseAuth.getInstance();
+        HomeActivity = new Intent(this, com.example.fetcipe.Activities.HomeActivity.class);
+
+        loginProgress.setVisibility(View.INVISIBLE);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginProgress.setVisibility(View.VISIBLE);
+                btnLogin.setVisibility(View.INVISIBLE);
+
+                final String mail = userMail.getText().toString();
+                final String password = userPassword.getText().toString();
+
+                if (mail.isEmpty() || password.isEmpty()) {
+                    showMessage("Please verify all fields");
+                } else {
+                    signIn(mail, password);
+                }
+            }
+        });
+    }
+
+    private void signIn(String mail, String password) {
+
+        mAuth.signInWithEmailAndPassword(mail, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+
+                    loginProgress.setVisibility(View.INVISIBLE);
+                    btnLogin.setVisibility(View.VISIBLE);
+                    updateUI();
+                } else
+                    showMessage(task.getException().getMessage());
+
+            }
+        });
+
+
+    }
+
+    private void updateUI() {
+
+        startActivity(HomeActivity);
+        finish();
+
+
+    }
+
+    private void showMessage(String text) {
+
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+    }
+}
