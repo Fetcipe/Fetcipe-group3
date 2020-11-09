037f0705d560c1aae7f15b56ff3a4331b33a4520
Thu Tran 19bit <1959039@itec.hcmus.edu.vn>
2020-11-09 23:14:17 +0700

    fix ggl service

--- app/src/main/java/com/example/fetcipe/Activities/LoginActivity.java
+++ app/src/main/java/com/example/fetcipe/Activities/LoginActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -16,6 +17,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -24,6 +26,7 @@ public class LoginActivity extends AppCompatActivity {
     private ProgressBar loginProgress;
     private FirebaseAuth mAuth;
     private Intent HomeActivity;
+    private ImageView loginPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +39,17 @@ public class LoginActivity extends AppCompatActivity {
         loginProgress = findViewById(R.id.login_progress);
         mAuth = FirebaseAuth.getInstance();
         HomeActivity = new Intent(this, com.example.fetcipe.Activities.HomeActivity.class);
+        loginPhoto = findViewById(R.id.login_photo);
+
+        loginPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent registerActivity = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(registerActivity);
+                finish();
+            }
+        });
 
         loginProgress.setVisibility(View.INVISIBLE);
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -49,6 +63,8 @@ public class LoginActivity extends AppCompatActivity {
 
                 if (mail.isEmpty() || password.isEmpty()) {
                     showMessage("Please verify all fields");
+                    btnLogin.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
                 } else {
                     signIn(mail, password);
                 }
@@ -67,9 +83,11 @@ public class LoginActivity extends AppCompatActivity {
                     loginProgress.setVisibility(View.INVISIBLE);
                     btnLogin.setVisibility(View.VISIBLE);
                     updateUI();
-                } else
+                } else {
                     showMessage(task.getException().getMessage());
-
+                    btnLogin.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
+                }
             }
         });
 
@@ -87,5 +105,16 @@ public class LoginActivity extends AppCompatActivity {
     private void showMessage(String text) {
 
         Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user != null) {
+            //user is already connected so we redirect him to homepage
+            updateUI();
+        }
     }
 }
